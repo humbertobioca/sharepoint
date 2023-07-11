3dd4f3f170830879920d08afe34394938742833b
Humberto Matheus Porto Bioca Alves <humberto.bioca@avanade.com>
2023-07-11 15:57:44 -0300

    refatorando o código.

---
+++ SharepointWorker/ListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharepointWorker
+{
+    public static class ListExtensions
+    {
+        public static IEnumerable<List<T>> Batch<T>(this List<T> list, int batchSize)
+        {
+            for (int i = 0; i < list.Count; i += batchSize)
+                yield return list.GetRange(i, Math.Min(batchSize, list.Count - i));
+        }
+    }
+
+}
--- SharepointWorker/Worker.cs
+++ SharepointWorker/Worker.cs
@@ -1,13 +1,11 @@
-using Cronos;
 using Microsoft.Graph;
 using Microsoft.Identity.Client;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
-using System.IO;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
+using static Microsoft.Graph.Constants;
 
 namespace SharepointWorker
 {
@@ -22,158 +20,177 @@ namespace SharepointWorker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-
-            // Authentication
-            //var app = ConfidentialClientApplicationBuilder.Create("694e9844-f1f6-4ea5-ab2b-43e2e7bddd6b")
-            //    .WithClientSecret("UWW8Q~WYrY3QzNgRI55wXmOebSVsBinItsC_iaTP")
-            //    .WithAuthority(AzureCloudInstance.AzurePublic, "0ff63586-3b7a-4f81-a766-7409f1ba5ae7")
-            //    .Build();
-
-            
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
 
                 try
                 {
-
                     var app = ConfidentialClientApplicationBuilder.Create("a06c2c16-7079-463a-9c44-3bba0c62016c")
                         .WithClientSecret("STh8Q~AB3C00lwL_D6A5DQrSzmiBuju.035MMbaE")
                         .WithAuthority(AzureCloudInstance.AzurePublic, "dcc867ff-f3cc-40f3-8e63-c33b8872a692")
                         .Build();
 
-                    List<string> scopes = new List<string>
-                    {
-                        "https://graph.microsoft.com/.default"
-                    };
-
+                    var scopes = new List<string> { "https://graph.microsoft.com/.default" };
                     var result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
-
                     var accessToken = result.AccessToken;
 
-                    string siteUrl = "https://graph.microsoft.com/v1.0/sites/vx65k.sharepoint.com:/sites/fibrasil";
-                    HttpClient client = new HttpClient();
-                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, siteUrl);
-                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-                    HttpResponseMessage response = await client.SendAsync(request);
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    var site = JsonConvert.DeserializeObject<dynamic>(responseBody);
-                    string siteId = site.id;
+                    string siteId = await GetSiteId(accessToken, "https://graph.microsoft.com/v1.0/sites/vx65k.sharepoint.com:/sites/fibrasil");
+                    string driveId = await GetDriveId(accessToken, $"https://graph.microsoft.com/v1.0/sites/{siteId}/drives");
+                    string itemId = await GetItemId(accessToken, $"https://graph.microsoft.com/v1.0/sites/{siteId}/drives/{driveId}/root:/Teste/Book.xlsx");
+                    var rangeValues = await GetRangeValues(accessToken, $"https://graph.microsoft.com/v1.0/sites/{siteId}/drives/{driveId}/items/{itemId}/workbook/worksheets/Sheet1/usedRange");
 
-                    string driveUrl = $"https://graph.microsoft.com/v1.0/sites/{siteId}/drives";
-                    request = new HttpRequestMessage(HttpMethod.Get, driveUrl);
-                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-                    response = await client.SendAsync(request);
-                    responseBody = await response.Content.ReadAsStringAsync();
-                    var drives = JsonConvert.DeserializeObject<dynamic>(responseBody);
-                    string driveId = drives.value[0].id;  // supondo que o arquivo está no primeiro drive listado
-
-                    string filePath = "/Teste/Book.xlsx";  // caminho relativo ao arquivo no SharePoint
-                    string itemUrl = $"https://graph.microsoft.com/v1.0/sites/{siteId}/drives/{driveId}/root:/{filePath}";
-                    request = new HttpRequestMessage(HttpMethod.Get, itemUrl);
-                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-                    response = await client.SendAsync(request);
-                    responseBody = await response.Content.ReadAsStringAsync();
-                    var item = JsonConvert.DeserializeObject<dynamic>(responseBody);
-                    string itemId = item.id;
-
-                    string sheetName = "Sheet1";  // nome da planilha
-                    string rangeUrl = $"https://graph.microsoft.com/v1.0/sites/{siteId}/drives/{driveId}/items/{itemId}/workbook/worksheets/{sheetName}/usedRange";
-                    request = new HttpRequestMessage(HttpMethod.Get, rangeUrl);
-                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-                    response = await client.SendAsync(request);
-                    responseBody = await response.Content.ReadAsStringAsync();
-                    var range = JsonConvert.DeserializeObject<dynamic>(responseBody);
-                    JArray rangeValues = range.text;
-                    int statusColumnIndex = -1;
-                    for (int i = 0; i < ((JArray)rangeValues[0]).Count; i++)
-                    {
-                        if (rangeValues[0][i].ToString() == "status")
-                        {
-                            statusColumnIndex = i;
-                            break;
-                        }
-                    }
+                    int statusColumnIndex = GetStatusColumnIndex(rangeValues);
 
                     if (statusColumnIndex == -1)
                     {
                         throw new Exception("Coluna 'status' não encontrada");
                     }
 
-                    List<int> rowsToProcess = new List<int>();
-                    for (int i = 1; i < rangeValues.Count; i++)  // começa em 1 para pular a linha de cabeçalho
-                    {
-                        if (rangeValues[i][statusColumnIndex].ToString() == "A PROCESSAR")
-                        {
-                            rowsToProcess.Add(i);
-                        }
-                    }
-
-                    int batchSize = 20;  // definir o tamanho do lote como 20
-                    int totalBatches = (int)Math.Ceiling((double)rowsToProcess.Count / batchSize);  // calculando o total de lotes
-
-                    for (int i = 0; i < totalBatches; i++)
-                    {
-                        List<string> requests = new List<string>();
-                        for (int j = i * batchSize; j < Math.Min(rowsToProcess.Count, (i + 1) * batchSize); j++)
-                        {
-                            int row = rowsToProcess[j];
-                            string columnLetter = ColumnLetter(statusColumnIndex);
-                            string cellAddress = $"{columnLetter}{row + 1}";
-                            string updateUrl = $"/sites/{siteId}/drives/{driveId}/items/{itemId}/workbook/worksheets/{sheetName}/range(address='{cellAddress}')";
-
-                            var updateValues = new
-                            {
-                                values = new string[][]
-                                {
-                                    new string[] { "EM PROCESSAMENTO" }
-                                }
-                            };
-                            var updateValuesJson = JsonConvert.SerializeObject(updateValues);
-                            string requestContent = $"{{\"id\":\"{j}\",\"method\":\"PATCH\",\"url\":\"{updateUrl}\",\"headers\":{{\"Content-Type\":\"application/json\"}},\"body\":{updateValuesJson}}}";
-                            requests.Add(requestContent);
-                        }
-                        string batchContent = $"{{\"requests\":[{string.Join(",", requests)}]}}";
-                        HttpRequestMessage batchRequest = new HttpRequestMessage(HttpMethod.Post, "https://graph.microsoft.com/v1.0/$batch")
-                        {
-                            Content = new StringContent(batchContent, Encoding.UTF8, "application/json")
-                        };
-                        batchRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-                        HttpResponseMessage batchResponse = await client.SendAsync(batchRequest);
-                        string batchResponseContent = await batchResponse.Content.ReadAsStringAsync();
-                        var batchResponseJson = JsonConvert.DeserializeObject<dynamic>(batchResponseContent);
-                        foreach (var batchResponset in batchResponseJson.responses)
-                        {
-                            if (batchResponset.status >= 400)
-                            {
-                                Console.WriteLine($"Request failed: {batchResponset.status} {batchResponset.body.error.message}");
-                            }
-                        }
-                    }
+                    var rowsToProcess = GetRowsToProcess(rangeValues, statusColumnIndex);
+                    await UpdateCellsInBatches(accessToken, rowsToProcess, statusColumnIndex, siteId, driveId, itemId, stoppingToken);
                 }
                 catch (Exception ex)
                 {
                     Console.Error.WriteLine($"Error uploading file: {ex.Message}");
                 }
-                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
 
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
             }
         }
 
-        public static string ColumnLetter(int intCol)
+        private async Task<string> GetSiteId(string accessToken, string siteUrl)
         {
-            intCol++;
-            var intFirstLetter = ((intCol) / 676) + 64;
-            var intSecondLetter = ((intCol % 676) / 26) + 64;
-            var intThirdLetter = (intCol % 26) + 65;
+            return await GetIdFromUrl(accessToken, siteUrl);
+        }
 
-            var firstLetter = (intFirstLetter > 64) ? (char)intFirstLetter : ' ';
-            var secondLetter = (intSecondLetter > 64) ? (char)intSecondLetter : ' ';
-            var thirdLetter = (char)intThirdLetter;
+        private async Task<string> GetDriveId(string accessToken, string driveUrl)
+        {
+            var response = await GetResponseFromUrl(accessToken, driveUrl);
+            var jsonObject = JsonConvert.DeserializeObject<dynamic>(response);
+            return jsonObject.value[0].id;
+        }
 
-            return string.Concat(firstLetter, secondLetter, thirdLetter).Trim();
+        private async Task<string> GetItemId(string accessToken, string itemUrl)
+        {
+            return await GetIdFromUrl(accessToken, itemUrl);
+        }
+
+        private async Task<string> GetIdFromUrl(string accessToken, string url)
+        {
+            var response = await GetResponseFromUrl(accessToken, url);
+            var jsonObject = JsonConvert.DeserializeObject<dynamic>(response);
+            return jsonObject.id;
+        }
+
+        private async Task<string> GetResponseFromUrl(string accessToken, string url)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await client.SendAsync(request);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private async Task<JArray> GetRangeValues(string accessToken, string rangeUrl)
+        {
+            var response = await GetResponseFromUrl(accessToken, rangeUrl);
+            var range = JsonConvert.DeserializeObject<dynamic>(response);
+            return range.text;
+        }
+
+        private int GetStatusColumnIndex(JArray rangeValues)
+        {
+            for (int i = 0; i < ((JArray)rangeValues[0]).Count; i++)
+            {
+                if (rangeValues[0][i].ToString().ToLower() == "status")
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private List<int> GetRowsToProcess(JArray rangeValues, int statusColumnIndex)
+        {
+            var rowsToProcess = new List<int>();
+            for (int i = 1; i < rangeValues.Count; i++)
+
+            {
+                if (rangeValues[i][statusColumnIndex].ToString().ToUpper() == "A PROCESSAR")
+                {
+                    rowsToProcess.Add(i);
+                }
+            }
+            return rowsToProcess;
+        }
+
+        private async Task UpdateCellsInBatches(string accessToken, List<int> rowsToProcess, int statusColumnIndex, string siteId, string driveId, string itemId, CancellationToken stoppingToken)
+        {
+            string sheetName = "Sheet1";
+            var client = new HttpClient();
+
+            foreach (List<int> batchRows in rowsToProcess.Batch(20))
+            {
+                string boundary = $"batch_{Guid.NewGuid()}";
+                MultipartContent batchContent = new MultipartContent("mixed", boundary);
+
+                foreach (int row in batchRows)
+                {
+                    string columnLetter = ColumnLetter(statusColumnIndex);
+                    string cellAddress = $"{columnLetter}{row + 1}";
+                    string updateUrl = $"https://graph.microsoft.com/v1.0/sites/{siteId}/drives/{driveId}/items/{itemId}/workbook/worksheets/{sheetName}/range(address='{cellAddress}')";
+                    var updateValues = new
+                    {
+                        values = new string[][]
+                        {
+                    new string[] { "EM PROCESSAMENTO" }
+                        }
+                    };
+                    HttpRequestMessage updateRequestMessage = new HttpRequestMessage(HttpMethod.Patch, updateUrl)
+                    {
+                        Content = new StringContent(JsonConvert.SerializeObject(updateValues), Encoding.UTF8, "application/json")
+                    };
+                    updateRequestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                    // Cria um novo conteúdo HttpMessageContent a partir da solicitação
+                    HttpMessageContent content = new HttpMessageContent(updateRequestMessage);
+                    // Adiciona o conteúdo da mensagem ao conteúdo do lote
+                    batchContent.Add(content);
+                }
+
+                HttpRequestMessage batchRequest = new HttpRequestMessage(HttpMethod.Post, "https://graph.microsoft.com/v1.0/$batch")
+                {
+                    Content = batchContent
+                };
+                batchRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                HttpResponseMessage batchResponse = await client.SendAsync(batchRequest);
+                string batchResponseContent = await batchResponse.Content.ReadAsStringAsync();
+                var batchResult = JsonConvert.DeserializeObject<BatchResponseContent>(batchResponseContent);
+
+                // Verifique os resultados da solicitação de lote para garantir que todas as atualizações foram bem-sucedidas.
+
+                await Task.Delay(TimeSpan.FromSeconds(60), stoppingToken);  // aguarde 60 segundos antes de enviar o próximo lote
+            }
         }
 
 
+
+        private static string ColumnLetter(int columnIndex)
+        {
+            int dividend = columnIndex + 1;
+            string columnName = String.Empty;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                columnName = Convert.ToChar(65 + modulo).ToString() + columnName;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+
+            return columnName;
+        }
     }
 }
